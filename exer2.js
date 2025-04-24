f58c439627660d6782454b06b7ffc38a199c56b9
yasmin1653 <yasmimvitoriapaiano@gmail.com>
2025-04-24 10:11:30 -0400

    24/4/2025

---
+++ exer2.js
@@ -0,0 +1,26 @@
+//1
+let n1 = 10;
+let n2 = 2;
+console.log("SOMA: 10 + 2 =" ,n1 + n2)
+console.log("SUBTRAÇÃO: 10 - 2 =", n1 - n2)
+console.log("MULTIPLICAÇÃO: 10 * 2 =", n1 * n2)
+console.log("DIVISÃO: 10 / 2 =", n1 / n2)
+
+//2
+let pessoa1= 9;
+let pessoa2= 13;
+let mensagem = pessoa1>=pessoa2? "Maior de idade" : "Menor de idade";
+console.log(mensagem);
+
+//3
+let pessoa= 29;
+let pessoa_2= 15;
+let mensagens = pessoa>=18? "Maior de idade" : "Menor de idade";
+console.log("A 1 pessoa é", mensagens);
+let mensagens2 = pessoa1>=18? "Maior de idade" : "Menor de idade";
+console.log("A 2 pessoa é", mensagens2);
+
+//4
+let variavel = 6;
+let varialvel = variavel % 2==0? "par" : "impar";
+console.log(varialvel);
