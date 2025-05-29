24cf1d32362ec05d229b30f8f84afeb13287107f
yasmin1653 <yasmimvitoriapaiano@gmail.com>
2025-05-29 08:06:36 -0400

    oi

---
+++ EstruturaRepetição_5.js
@@ -0,0 +1,11 @@
+import rl from 'readline-sync';
+
+//ATIVIDADE 1
+let num = rl.question("Informe o número pera gerar a tabuada: ");
+let cont = 1;
+while (cont <= 10) {
+    console.log(`${num} x ${cont} = ${num * cont}`)
+}
+
+//ATIVIDADE 2
+
